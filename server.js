c3256f4275126f36fc43c627b2639e3acffb4781
rahoolrathi <rahoolrathi12@gmail.com>
2024-07-09 23:23:24 +0500

    Connected Database and Created Project Structure

---
+++ server.js
@@ -0,0 +1,16 @@
+const dotenv=require('dotenv')
+const app=require('./app')
+const mongoose=require('mongoose')
+dotenv.config({path:'./config.env'})
+
+
+const port =process.env.PORT||3000
+
+mongoose.connect(process.env.DATABASE_URL).then(()=>{
+    console.log('Database Connected');
+    app.listen(3000,()=>{
+        console.log(`server is runing on port ${port}`)
+    })
+}).catch(()=>console.log("Error in connecting database"))
+
+
